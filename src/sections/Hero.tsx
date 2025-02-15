21682ef998897b70b6a63b092d72076d27ca0ba5
iLLiSiO2 <samsudar200@gmail.com>
2025-02-15 14:36:02 +0530

    Hero Section - I

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,44 @@
+import memojiImage from "@/assets/images/memoji-computer.png";
+import Image from "next/image";
+import ArrowDown from "@/assets/icons/arrow-down.svg";
+
 export const HeroSection = () => {
-  return <div>Hero Section</div>;
+  return (
+    <div className="py-32">
+      <div className="container">
+        <div className="flex flex-col items-center">
+          <Image
+            src={memojiImage}
+            className="size-[100px]"
+            alt="Person peeking from behind laptop"
+          />
+          <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
+            <div className="bg-green-500 size-2.5 rounded-full"></div>
+            <div className="text-sm font-medium">
+              Available for new projects
+            </div>
+          </div>
+        </div>
+        <h1 className="font-serif text-3xl text-center mt-8 tracking-wide">
+          Building Exceptional User Experiences
+        </h1>
+        <p className="mt-4 text-center text-white/60">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book
+        </p>
+        <div className="flex flex-col items-center">
+          <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+            <span className="font-semibold">Explore My Work</span>
+            <ArrowDown className="size-4" />
+          </button>
+          <button>
+            <span>✋</span>
+            <span>Lets Connect</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
