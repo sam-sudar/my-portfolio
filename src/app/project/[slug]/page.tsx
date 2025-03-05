b3eed51afc7ce1f38ce40afb5b609b07e103d1e0
iLLiSiO2 <samsudar200@gmail.com>
2025-03-05 20:05:41 +0530

    Routing implementation

---
+++ src/app/project/[slug]/page.tsx
@@ -0,0 +1,7 @@
+export default async function Page() {
+  return (
+    <ul>
+      <h1>Indi project</h1>
+    </ul>
+  );
+}
---
+++ src/app/project/page.tsx
@@ -0,0 +1,7 @@
+export default async function Page() {
+  return (
+    <ul>
+      <h1>Hello bruh</h1>
+    </ul>
+  );
+}
